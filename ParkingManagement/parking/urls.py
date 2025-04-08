c459c4d4089cd71490db92080e7dbc554141903c
Bhanu Prakash <147345461+Bhanu7569@users.noreply.github.com>
2025-04-08 12:38:58 +0530

    Delete ParkingManagement directory

--- ParkingManagement/parking/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns= [
-    path('register/',views.register_slot,name='register_slot'),
-    path('unregiter/',views.unregister_slot,name='unregister_slot'),
-
-]
-
